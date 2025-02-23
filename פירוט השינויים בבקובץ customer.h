cad2b8801accb4a98cbc9c58ad7ca72ac55d9a43
Dorkr33 <dorkrinizky2000@gmail.com>
2025-02-23 23:06:26 +0200

    Update פירוט השינויים בבקובץ customer.h

--- פירוט השינויים בבקובץ customer.h
+++ פירוט השינויים בבקובץ customer.h
@@ -1,4 +1,3 @@
-
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
@@ -13,26 +12,27 @@ typedef struct Purchase {
 
 // מבנה לקוח
 typedef struct Customer {
-    char name[100];            // שם לקוח
-    struct tm joinDate;        // תאריך הצטרפות (מבנה struct tm)
-    Purchase *purchases;       // רשימת ספרים שנרכשו על ידי הלקוח
+    char name[100];            // שם הלקוח
+    struct tm joinDate;        // תאריך ההצטרפות (מבנה struct tm)
+    Purchase* purchases;       // רשימת הספרים שנרכשו על ידי הלקוח
     struct Customer* next;     // מצביע ללקוח הבא ברשימה המקושרת
 } Customer;
 
-// פרוטוטייפים לפונקציות הקשורות ללקוח
+// פרוטוטייפים לפונקציות הקשורות לניהול לקוחות
 Customer* createCustomer(const char* name, const struct tm* joinDate);
-void loadCustomerData(const char* filename, Customer** head);
+void loadCustomerData(const char* filename, Customer** head); // במידת הצורך
 void saveCustomerData(const char* filename, Customer* head);
 void addCustomer(Customer** head, Customer* newCustomer);
 void freeList(Customer* head);
-void inputCustomerData(Customer** head);  // קלט נתוני לקוח חדש
+void inputCustomerData(Customer** head);
 
-// פונקציות בדיקה
-int isValidName_customer(const char* name);       // בדיקת שם לקוח תקין (אותיות אנגליות ורווחים בלבד)
+// פונקציות בדיקה לקלט
+int isValidName_customer(const char* name);       // בדיקת שם לקוח תקין (אותיות אנגלית ורווחים בלבד)
 int isValidDate_cusromer(const char* date);         // בדיקת תאריך תקין (פורמט DD/MM/YYYY)
 
-// פונקציות תצוגה וניהול רכישות
+// פונקציות להצגת לקוחות וניהול רכישות
 void viewCustomers(Customer* head);
-void purchaseBookForCustomer(Customer **head);     // פונקציה להזנת רכישת ספר ללקוח
+void addPurchase(Customer* customer, const char* bookTitle);
+void purchaseBookForCustomer(Customer** head);
 
 #endif
